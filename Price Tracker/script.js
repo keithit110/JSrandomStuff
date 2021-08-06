1fe4e068096553fe2a5f4d1fc1e7eeb470f5b2c5
Keith Rivera <31339871+keithit110@users.noreply.github.com>
2021-08-06 19:21:40 -0400

    Update script.js
    - added 10 second refresh
    - added html arrow entity in tab to indicate price went down or up in the last 10 seconds

--- Price Tracker/script.js
+++ Price Tracker/script.js
@@ -3,18 +3,32 @@ const coin = 'SLP';
 let ws = new WebSocket(`wss://stream.binance.com:9443/ws/${coinPair}@trade`);
 let stockPriceElement = document.getElementById('stock-price');
 let lastPrice = null;
+let price =null;
 
-ws.onmessage = (event) => {
+  ws.onmessage = function(event) {
   let stockObject = JSON.parse(event.data);
-  //   console.log(stockObject);
-  let price = parseFloat(stockObject.p).toFixed(6); //decimal precision
-  stockPriceElement.innerText = price;
-  stockPriceElement.style.color =
-    !lastPrice || lastPrice === price
-      ? 'black'
-      : price > lastPrice
-      ? 'green'
-      : 'red';
-  lastPrice = price;
-  document.title = `${coin}: $${price}`;
-};
+    // console.log(stockObject);
+  price = parseFloat(stockObject.p).toFixed(6); //decimal precision 
+
+}
+
+function setSymbol(s1,s2,s3){
+  return !lastPrice || lastPrice === price
+       ? s1
+       : price > lastPrice
+       ? s2
+       : s3
+ }
+
+const setPrice =function(){
+  stockPriceElement.innerText = `${price}`;
+  stockPriceElement.style.color = setSymbol('black','green','red')    
+      document.title = `${setSymbol('\u2014','\u2191','\u2193')} ${coin}: $${price}`;
+      lastPrice = price;
+
+}
+
+
+
+setInterval(setPrice, 10000);
+
