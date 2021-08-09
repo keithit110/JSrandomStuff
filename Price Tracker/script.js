98fc02079de45183dc3f0a5792bff174fe9158f7
Keith Rivera <31339871+keithit110@users.noreply.github.com>
2021-08-09 10:51:02 -0400

    change
    -data polls every 10secs
    -accept browser prompts now to track usdt pair

--- Price Tracker/script.js
+++ Price Tracker/script.js
@@ -1,34 +1,32 @@
-const coinPair = 'slpusdt'; //must be lower case
-const coin = 'SLP';
-let ws = new WebSocket(`wss://stream.binance.com:9443/ws/${coinPair}@trade`);
+// const coinPair = 'slpusdt'; //must be lower case
+const coin = prompt(
+  'Input coin ticker you want to track (ex. btc):'
+).toLowerCase();
+console.log(coin);
+let ws = new WebSocket(`wss://stream.binance.com:9443/ws/${coin}usdt@trade`);
 let stockPriceElement = document.getElementById('stock-price');
 let lastPrice = null;
-let price =null;
+let price = null;
 
-  ws.onmessage = function(event) {
+ws.onmessage = function (event) {
   let stockObject = JSON.parse(event.data);
-    // console.log(stockObject);
-  price = parseFloat(stockObject.p).toFixed(5); //decimal precision 
+  // console.log(stockObject);
+  price = parseFloat(stockObject.p).toFixed(5); //decimal precision
+};
 
+function setSymbol(s1, s2, s3) {
+  return !lastPrice || lastPrice === price ? s1 : price > lastPrice ? s2 : s3;
 }
 
-function setSymbol(s1,s2,s3){
-  return !lastPrice || lastPrice === price
-       ? s1
-       : price > lastPrice
-       ? s2
-       : s3
- }
-
-const setPrice =function(){
+const setPrice = function () {
   stockPriceElement.innerText = `${price}`;
-  stockPriceElement.style.color = setSymbol('black','green','red')    
-      document.title = `${setSymbol('\u2014','\u2191','\u2193')} ${coin}: $${price}`;
-      lastPrice = price;
-
-}
-
-
+  stockPriceElement.style.color = setSymbol('black', 'green', 'red');
+  document.title = `${setSymbol(
+    '\u2014',
+    '\u2191',
+    '\u2193'
+  )} ${coin}: $${price}`;
+  lastPrice = price;
+};
 
 setInterval(setPrice, 10000);
-
